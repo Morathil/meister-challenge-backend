ad166df210e1a79a7558ef9bd5b29b2a1a9fbc7c
Christoph Altmann <christoph.altmann@senseven.ai>
2024-12-22 18:11:54 +0100

    Remove rails.logger and extended name for task creation job

--- app/jobs/create_task_job.rb
+++ app/jobs/create_task_job.rb
@@ -4,8 +4,7 @@ class CreateTaskJob
 
   def perform(*args)
     Project.find_each do |p|
-      Task.create(name: "New Task #{Time.zone.now} #{p.id}", project: p)
-      Rails.logger.info "Created a new task. #{p.id}"
+      Task.create(name: "New Task #{Time.zone.now}", project: p)
     end
   end
 end
